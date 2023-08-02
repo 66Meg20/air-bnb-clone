756f801ee7bf0708798a5d35e431ff4b523004a4
66Meg20 <66meg.dj2066@gmail.com>
2023-08-02 01:43:02 -0500

    added header file and icons

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+import AirbnbLogo from "./assets/AirbnbLogo.png";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
+
+export default function Header() {
+  return (
+    <div className="header__container">
+      <img src={AirbnbLogo} alt="logo" />
+      <div className="header__center">
+        <input type="search" />
+        <SearchOutlinedIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageOutlinedIcon />
+        <ArrowDropDownOutlinedIcon />
+        <AccountCircleOutlinedIcon />
+      </div>
+    </div>
+  );
+}
