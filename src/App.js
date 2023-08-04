6f9f0dd082798fe4994545542770cfc41e6e6e7e
66Meg20 <66meg.dj2066@gmail.com>
2023-08-04 14:21:11 -0500

    added the details for the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header.js";
 import Home from "./components/Home.js";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       {/* <Calendar/> */}
       <Home />
       {/* <Cards/> */}
-      {/* <Footer/> */}
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer__container">
+      <p>2022 Airbnb clone! No rites reserved - this is demo!</p>
+      <p>Privacy • Terms • Sitemap • Companu details</p>
+    </div>
+  );
+}
